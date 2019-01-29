afb1c3ed52915d10c4c86195b9bc65df347de5ff
Federico Boschini <federico@nearit.com>
2019-01-29 12:30:34 +0100

    fix(hooks): check if platform exists before injecting values

--- hooks/after_prepare_android.js
+++ hooks/after_prepare_android.js
@@ -47,101 +47,105 @@ var platformDir = path.join(rootdir, 'platforms', 'android');
 var resourcesDir = path.join(rootdir, 'resources', 'android');
 var manifestFile = path.join(platformDir, 'AndroidManifest.xml');
 
-if (!fs.existsSync(manifestFile)) {
+if (platformDir) {
+  if (!fs.existsSync(manifestFile)) {
     // Probably Cordova-Android 7.X.X, search deeper
     manifestFile = path.join(platformDir, 'app/src/main/AndroidManifest.xml');
-}
-
-if (!fs.existsSync(manifestFile)) {
-  throw new Error("! Can't find the AndroidManifest.xml. This shouldn't happen, please contact us for support.\n")
-}
-
-/**
- * Inject `near_api_key` from Cordova `config.xml` into Android manifest
- */
-var apiKey = lib.searchPreferenceByName(rootdir, 'android', 'near_api_key');
-
-if (apiKey) {
-  var tempManifest = lib.parseElementtreeSync(manifestFile);
-  var root = tempManifest.getroot();
-
-  var nearApiKeyElm = root.find("application/meta-data[@android:name='near_api_key']")
-  if (nearApiKeyElm) {
-    nearApiKeyElm.attrib['android:value'] = apiKey
-  } else {
-    console.log(`* 'near_api_key' meta not found. Will try to add\n`)
-    try {
-      var apiKeyMeta = et.Element('meta-data', {
-        'android:name': 'near_api_key',
-        'android:value': apiKey,
-      })
-
-      root.find('application')
-        .append(apiKeyMeta)
-    } catch (e) {
-      throw new Error("! Can't inject `near_api_key` meta inside AndroidManifest.xml. This shouldn't happen, please contact us for support.\n")
-    }
   }
 
-  fs.writeFileSync(manifestFile, tempManifest.write({ indent: 4 }), 'utf-8');
-  console.log(`* 'near_api_key' correctly added to AndroidManifest.xml\n`)
-} else {
-  throw new Error("! Missing `near_api_key` in your `config.xml`\n");
-}
+  if (!fs.existsSync(manifestFile)) {
+    throw new Error("! Can't find the AndroidManifest.xml. This shouldn't happen, please contact us for support.\n")
+  }
 
-/**
- * Inject `near_url_scheme` from Cordova `config.xml` into Android manifest
- */
-var urlScheme = lib.searchPreferenceByName(rootdir, 'android', 'near_url_scheme');
+  /**
+   * Inject `near_api_key` from Cordova `config.xml` into Android manifest
+   */
+  var apiKey = lib.searchPreferenceByName(rootdir, 'android', 'near_api_key');
 
-if (urlScheme) {
-  var tempManifest = lib.parseElementtreeSync(manifestFile);
-  var root = tempManifest.getroot();
-  var testDeviceActivityElm = root.find("application/activity[@android:name='it.near.sdk.utils.device.NearTestEnrollActivity']")
-  if (testDeviceActivityElm) {
-    var intentFilter = testDeviceActivityElm.find("intent-filter")
-    if (intentFilter) {
-      var entries = intentFilter.getchildren()
-      for (var i = 0; i < entries.length; i++) {
-        if (entries[i].tag == "data") {
-          if (entries[i].attrib['android:scheme']) {
-            entries[i].attrib['android:scheme'] = urlScheme
+  if (apiKey) {
+    var tempManifest = lib.parseElementtreeSync(manifestFile);
+    var root = tempManifest.getroot();
+
+    var nearApiKeyElm = root.find("application/meta-data[@android:name='near_api_key']")
+    if (nearApiKeyElm) {
+      nearApiKeyElm.attrib['android:value'] = apiKey
+    } else {
+      console.log(`* 'near_api_key' meta not found. Will try to add\n`)
+      try {
+        var apiKeyMeta = et.Element('meta-data', {
+          'android:name': 'near_api_key',
+          'android:value': apiKey,
+        })
+
+        root.find('application')
+          .append(apiKeyMeta)
+      } catch (e) {
+        throw new Error("! Can't inject `near_api_key` meta inside AndroidManifest.xml. This shouldn't happen, please contact us for support.\n")
+      }
+    }
+
+    fs.writeFileSync(manifestFile, tempManifest.write({ indent: 4 }), 'utf-8');
+    console.log(`* 'near_api_key' correctly added to AndroidManifest.xml\n`)
+  } else {
+    throw new Error("! Missing `near_api_key` in your `config.xml`\n");
+  }
+
+  /**
+   * Inject `near_url_scheme` from Cordova `config.xml` into Android manifest
+   */
+  var urlScheme = lib.searchPreferenceByName(rootdir, 'android', 'near_url_scheme');
+
+  if (urlScheme) {
+    var tempManifest = lib.parseElementtreeSync(manifestFile);
+    var root = tempManifest.getroot();
+    var testDeviceActivityElm = root.find("application/activity[@android:name='it.near.sdk.utils.device.NearTestEnrollActivity']")
+    if (testDeviceActivityElm) {
+      var intentFilter = testDeviceActivityElm.find("intent-filter")
+      if (intentFilter) {
+        var entries = intentFilter.getchildren()
+        for (var i = 0; i < entries.length; i++) {
+          if (entries[i].tag == "data") {
+            if (entries[i].attrib['android:scheme']) {
+              entries[i].attrib['android:scheme'] = urlScheme
+            }
           }
         }
       }
+    } else {
+      console.log(`* 'NearTestEnrollActivity' not found. Will try to add\n`)
+      try {
+        var action = et.Element('action', {
+          'android:name': 'android.intent.action.VIEW'
+        })
+        var category1 = et.Element('category', {
+          'android:name': 'android.intent.category.DEFAULT'
+        })
+        var category2 = et.Element('category', {
+          'android:name': 'android.intent.category.BROWSABLE'
+        })
+        var data = et.Element('data', {
+          'android:scheme': urlScheme
+        })
+        var testDevActivity = et.Element('activity', {
+          'android:name': 'it.near.sdk.utils.device.NearTestEnrollActivity',
+          'android:theme': '@style/NearTestEnrollStyle'
+        })
+        var intentFilter = et.Element('intent-filter')
+        intentFilter.append(action)
+        intentFilter.append(category1)
+        intentFilter.append(category2)
+        intentFilter.append(data)
+        testDevActivity.append(intentFilter)
+        root.find('application').append(testDevActivity)
+      } catch (e) {
+        console.log("! Can't inject `near_url_scheme` inside AndroidManifest.xml. This shouldn't happen, please contact us for support.\n")
+      }
     }
+    fs.writeFileSync(manifestFile, tempManifest.write({ indent: 4 }), 'utf-8');
+    console.log(`* 'near_url_scheme' correctly added to AndroidManifest.xml\n`)
   } else {
-    console.log(`* 'NearTestEnrollActivity' not found. Will try to add\n`)
-    try {
-      var action = et.Element('action', {
-        'android:name': 'android.intent.action.VIEW'
-      })
-      var category1 = et.Element('category', {
-        'android:name': 'android.intent.category.DEFAULT'
-      })
-      var category2 = et.Element('category', {
-        'android:name': 'android.intent.category.BROWSABLE'
-      })
-      var data = et.Element('data', {
-        'android:scheme': urlScheme
-      })
-      var testDevActivity = et.Element('activity', {
-        'android:name': 'it.near.sdk.utils.device.NearTestEnrollActivity',
-        'android:theme': '@style/NearTestEnrollStyle'
-      })
-      var intentFilter = et.Element('intent-filter')
-      intentFilter.append(action)
-      intentFilter.append(category1)
-      intentFilter.append(category2)
-      intentFilter.append(data)
-      testDevActivity.append(intentFilter)
-      root.find('application').append(testDevActivity)
-    } catch (e) {
-      console.log("! Can't inject `near_url_scheme` inside AndroidManifest.xml. This shouldn't happen, please contact us for support.\n")
-    }
+    console.log("! Missing `near_url_scheme` in your `config.xml`\n")
   }
-  fs.writeFileSync(manifestFile, tempManifest.write({ indent: 4 }), 'utf-8');
-  console.log(`* 'near_url_scheme' correctly added to AndroidManifest.xml\n`)
 } else {
-  console.log("! Missing `near_url_scheme` in your `config.xml`\n")
+  console.log("! Missing android platform. Skipping..")
 }
